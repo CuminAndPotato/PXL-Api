3406c8e8e2c806a01afaea6982214a0984aa61f5
SchlenkR <SchlenkR@github>
2025-01-30 08:20:33 +0100

    bump version to 0.0.2 and add App.fs to project

---
+++ dotnet/fsharp/Pxl.Ui/App.fs
@@ -0,0 +1,14 @@
+namespace Pxl
+
+open System
+
+[<AttributeUsage(AttributeTargets.All, AllowMultiple = false)>]
+type AppV1Attribute(name: string) =
+    inherit Attribute()
+    member _.name = name
+    member val defaultDurationInSeconds = 15.0 with get, set
+    member val displayName = "" with get, set
+    member val author = "" with get, set
+    member val description = "" with get, set
+    member val includeInCycle = true with get, set
+    member val isDebugApp = false with get, set
